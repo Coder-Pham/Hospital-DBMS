33578658c6b599e986b2934d9a12a0138343562e
Phạm Trọng Nhân <tendaigom@gmail.com>
2019-06-08 14:56:39 +0700

    Change files location

--- src/helpers/Info.java
+++
@@ -1,9 +0,0 @@
-package helpers;
-
-import java.sql.Connection;
-
-public class Info {
-    public static String username;
-    public static String password;
-    public static Connection connection;
-}
